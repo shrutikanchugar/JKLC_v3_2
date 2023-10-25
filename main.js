7e9249409b208b56b600c8f1d41f35340a349423
shrutikanchugar <68994100+shrutikanchugar@users.noreply.github.com>
2023-10-25 20:36:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function bio_page()
+{
+    window.location = "index.html";
+}
+
+function TLogin(){
+    window.location = "TLogin.html";
+}
+
+function SLogin(){
+    window.location = "SLogin.html";
+}
+
+function NewReg(){
+    window.location = "NewReg.html"
+}
